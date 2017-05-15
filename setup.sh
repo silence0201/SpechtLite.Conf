de4076de3a2c4d67ec36acfe541f7562e410692b
Silence <374619540@qq.com>
2017-05-15 10:23:02 +0800

    Update

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+# mv ~/Library/Developer/Xcode/UserData/CodeSnippets ~/Library/Developer/Xcode/UserData/CodeSnippets.backup
+
+SRC_HOME=`pwd`
+cp  ${SRC_HOME}/* ~/.SpechtLite
+echo "done"
